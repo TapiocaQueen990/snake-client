6aa83ccc1b31a782de4052e632b294a72a04ec0c
Daniel Pho <daniel.phou@hotmail.com>
2024-02-14 16:09:56 -0500

    added code to send name to server

--- client.js
+++ client.js
@@ -1,13 +1,24 @@
 const net = require("net");
+// const { Client } = require("undici-types");
 
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
     port: 50541,
   });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on("connect", () => {
+    conn.write("Name: PHO");
+  })
 conn.on("data", (data) => {
   console.log(`Server says ${data}`);
-})
+});
+
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
