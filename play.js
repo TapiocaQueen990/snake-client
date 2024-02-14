acadc6c8043747ed570f3cf89ce3a0027bed7bba
Daniel Pho <daniel.phou@hotmail.com>
2024-02-14 15:41:01 -0500

    removed body of function connect now it just is being imported

--- play.js
+++ play.js
@@ -1,19 +1,20 @@
 const net = require("net");
+const connect = require("./client.js")
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-conn.on("data", (data) => {
-  console.log(`Server says ${data}`);
-})
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "localhost",
+//     port: 50541,
+//   });
+// conn.on("data", (data) => {
+//   console.log(`Server says ${data}`);
+// })
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  return conn;
-};
+//   return conn;
+// };
 
 console.log("Connecting ...");
 connect();
