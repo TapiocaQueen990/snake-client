482843cc900ca6e9540f0fb895555edbefb6fe8f
Daniel Pho <daniel.phou@hotmail.com>
2024-02-14 19:46:09 -0500

    cleaned up code and moved functions toward input file

--- play.js
+++ play.js
@@ -1,22 +1,10 @@
 const net = require("net");
 const connect = require("./client.js");
-// const { stdin } = require("process");
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
+const setupInput = require("./input.js");
 
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
 
 console.log("Connecting ...");
 connect();
+
+setupInput();
