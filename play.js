090ecf4e8bced79e75d8a509d6f63838750023ae
Daniel Pho <daniel.phou@hotmail.com>
2024-02-14 19:26:04 -0500

    created code to exit application on detection of certain keys

--- play.js
+++ play.js
@@ -1,20 +1,22 @@
 const net = require("net");
-const connect = require("./client.js")
+const connect = require("./client.js");
+// const { stdin } = require("process");
 
-// establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: "localhost",
-//     port: 50541,
-//   });
-// conn.on("data", (data) => {
-//   console.log(`Server says ${data}`);
-// })
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
 
-//   return conn;
-// };
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
 
 console.log("Connecting ...");
 connect();
