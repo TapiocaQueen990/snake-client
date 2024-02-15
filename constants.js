d4f233fdc694c07c06f449d5dc192c3fe3d1433c
Daniel Pho <daniel.phou@hotmail.com>
2024-02-14 21:34:59 -0500

    cleaned up other modules and replaced variables

---
+++ constants.js
@@ -0,0 +1,27 @@
+const IP = "localhost"
+const PORT = 50541
+const MOVE_UP_KEY = "w"
+const MOVE_LEFT_KEY = "a"
+const MOVE_DOWN_KEY = "s"
+const MOVE_RIGHT_KEY = "d"
+
+const MESSAGES = {
+  '1': "Say: Whoops!",
+  '2': "Say: Nice Try",
+  '3': "Say: GG!",
+  '4': "Say: :P"
+};
+
+
+
+
+
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  MOVE_DOWN_KEY,
+  MESSAGES
+};
